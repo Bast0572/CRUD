8ba56b4311364b384496a97c4c69d90e6a556af4
Bast0572 <bastien.gaudin@angers.globalopencampus.com>
2023-10-23 11:57:36 +0200

    mise en place BD

--- config/database.js
+++ config/database.js
@@ -19,12 +19,39 @@ js-sha256
 
 const sqlite3 =require("sqlite3").verbose();
 
-const db
+const db = new sqlite3.Database('project_db')
+
+db.serialize(()=>{
+    db.run(`create table if not exists Teams
+    (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    )`)
+})
 
 db.serialize(()=> {
-    db.run('create table if not exists users (
-        id INTEGER PRIMARY KEY,
-        name TEXT 
+    db.run(`create table if not exists Users 
+        (id INTEGER PRIMARY KEY,
+        name TEXT,
+        pwd TEXT,
+        email TEXT,
+        created DATE,
+        updated DATE || null,
+        admin BOOL,
+        team INTEGER,
+        FOREIGN KEY(team) REFERENCES Teams(id)
+        )`
         )
+})
+
+db.serialize(()=> {
+    db.run(`create table if not exists User_Team(
+        user_id INTEGER,
+        team_id INTEGER,
+        PRIMARY KEY (user_id,team_id),
+        FOREIGN KEY (user_id) REFERENCES Users(id),
+        FOREIGN KEY (team_id) REFERENCES Teams(id)
+    )`
+    )
 })
 
