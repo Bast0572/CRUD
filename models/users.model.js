d2f53091f0666a1483e6ab7c4654db7a34663de8
Bast0572 <bastien.gaudin@angers.globalopencampus.com>
2023-11-21 11:51:06 +0100

    maj user model

--- models/users.model.js
+++ models/users.model.js
@@ -1,4 +1,19 @@
-const db =require("../config/database")
+
+/*
+
+    On récupère la base de données
+
+*/
+
+const db =require("../config/database.js")
+
+
+
+/*
+
+    Méthode récupérant tout les 'users'
+
+*/
 
 const getAll = () =>{
     const sql ="SELECT * FROM Users ORDER BY name"
@@ -11,10 +26,17 @@ const getAll = () =>{
         })
     })
 }
-//module.exports= {getAll}
+
+
+/*
+
+    Méthode récupérant un 'user' en fonction de son 'id'
+
+*/
+
 
 const getOne =(id) =>{
-    const sql ="SELECT * FROM Users WHERE name={id}"
+    const sql ="SELECT * FROM Users WHERE id={id}"
     db.get(sql,[id],(err,rows)=>{
         return new Promise((resolve,reject)=>{
             if (err){
@@ -25,7 +47,12 @@ const getOne =(id) =>{
     })
 
 }
-//module.exports={getOne}
+
+/*
+
+     Méthode pour ajouter un 'user'
+
+*/ 
 
 const add =(name,pwd,email,created,admin,team) =>{
     const sql ="INSERT INTO Users(name,pwd,email,created,admin,team) VALUES({name},{pwd},{email},{admin},{team}"
@@ -39,4 +66,44 @@ const add =(name,pwd,email,created,admin,team) =>{
         })
     })
 }
-module.exports={add,getOne,getAll}
+
+/*
+
+    Méthode pour modifier le 'nom'(name) d'un 'user' en fonction de son 'id'
+
+*/
+
+const update = (id,name) =>{
+    const sql ="update users set name={name} where id={id}"
+    db.exec(sql,[id,name],(err,rows)=>{
+        return new Promise((resolve,reject)=>{
+            if (err){
+                return reject(err)
+            }
+            return resolve(rows)
+        })
+    })
+}
+
+/*
+
+    Méthode pour supprimer un 'user' en fonction de son 'id'
+
+*/
+
+const deleteUser = (id)=> {
+    const sql= "delete from users where id ={id}"
+    db.exec(sql,[id],(err,rows) => {
+        return new Promise((resolve,reject)=>{
+            if(err){
+                return reject(err)
+            }
+
+            return resolve(rows)
+        })
+    })
+}
+
+// On exporte les différentes méthodes pour pouvoir les utiliser dans d'autre fichiers
+
+module.exports={add,getOne,getAll,update,deleteUser}
